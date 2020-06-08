953a727d2b8122eee6579f2178290693f0e01120
Archita Patil <architapatil@Architas-MacBook-Air.local>
2020-06-08 15:02:25 +0530

    Updates

---
+++ precache-manifest.88eff5dc1a9dbfc52c576893978c5069.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fba7a89d565ef7642977250fe035e1a3",
+    "url": "/smart-brain/index.html"
+  },
+  {
+    "revision": "151eaf260684c0d5ee5c",
+    "url": "/smart-brain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "6a48d20df8e8b4267bcb",
+    "url": "/smart-brain/static/css/main.257203cb.chunk.css"
+  },
+  {
+    "revision": "151eaf260684c0d5ee5c",
+    "url": "/smart-brain/static/js/2.61848096.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/smart-brain/static/js/2.61848096.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a48d20df8e8b4267bcb",
+    "url": "/smart-brain/static/js/main.bb8f5665.chunk.js"
+  },
+  {
+    "revision": "8a101b1990dccf6d9c53",
+    "url": "/smart-brain/static/js/runtime-main.9e863c25.js"
+  }
+]);
